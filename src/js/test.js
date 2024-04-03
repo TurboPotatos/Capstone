fa92b8f19716859ce9ff8c07302f0a233ba1662e
Jesse Woodruff <jwoodruff4@student.cvtc.edu>
2024-04-02 19:24:28 -0500

    Add a skeletal file structure

---
+++ src/js/test.js
@@ -0,0 +1,41 @@
+function init() {
+
+  class DOMAccess {
+    constructor(someParameterVariable, stuff) {
+      this.exampleProperty = someParameterVariable;
+      this.stuff = stuff;
+    }
+
+    logoPicCannon = document.querySelector("#zimmerArtillery");
+    testPic = document.querySelector("#testPic");
+
+    anotherProperty = {
+      property1: 12
+    };
+
+    stillAnotherProperty = 25;
+
+    doStuff() {
+      // let primaryContentDiv = document.querySelector("#primaryContent");
+
+      // primaryContentDiv.className = "joe";
+
+      // console.log(`last paragraph = ${document.querySelector("#motd > p:last-child").innerText}`);
+    };
+
+    static getClicked() {  
+      //console.log("your did it");
+      testPic.style.backgroundColor = "blue";
+    }
+  }
+
+  let domAccessClassInstance = new DOMAccess("Hello there", 50);
+
+  // console.log(`Message from DOMAccess class: ${domAccessClassInstance.exampleProperty} and stuff property ${domAccessClassInstance.stuff}`);
+
+  domAccessClassInstance.doStuff();
+
+  DOMAccess.testPic/addEventListener("click", DOMAccess.getClicked);
+}
+
+window.onload = init;
