edc3188ce08bbf0dfdd90120ee46a229b24db9bb
Erika Kennedy <ekennedy1@cvtc.edu>
2024-04-30 21:24:39 -0500

    separated css files, worked on dice faces

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
   <link href="src/css/styles.css" rel="stylesheet">
+  <link rel="stylesheet" href="src/css/signIn.css">
 
   <title>Henchmen Hospital</title>
 </head>
